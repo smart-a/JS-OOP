03095c5d033f98d4a0da53c37b00f82eb7314fe5
smart-a <olalekan.adesoye@gmail.com>
2021-01-28 15:42:38 +0100

    Intermediate function inheritance

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// Javacript Object
+
+// Super or Parent Constructor
 function Shape(color) {
   this.color = color;
 }
 
+// Intermediate function inheritance
+function extend(Parent, Child) {
+  // Inherite from Shape object
+  Child.prototype = Object.create(Parent.prototype);
+
+  // Restting contructor
+  Child.prototype.constructor = Child;
+}
+
 function Circle(radius, color) {
   this.radius = radius;
+
+  // Calling super constructor arg (Shape constructor)
   Shape.call(this, color);
 
   let defualtLocation = { x: 0, y: 0 };
@@ -16,6 +30,7 @@ function Circle(radius, color) {
     console.log("draw");
   };
 
+  // Configure variable with setter and getter
   Object.defineProperty(this, "defaultLocation", {
     get: function () {
       return defualtLocation;
@@ -26,13 +41,12 @@ function Circle(radius, color) {
   });
 }
 
+// Prototype method
 Shape.prototype.move = function () {
   console.log("move");
 };
 
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
-
+extend(Shape, Circle);
 const c = new Circle(1, "blue");
 c.draw();
 c.move();
